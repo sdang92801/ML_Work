66c2af2d03ceca04420e196902ec8a91f524f811
sdang92801 <77860420+sdang92801@users.noreply.github.com>
2021-01-24 06:16:36 -0800

    uncomment values_greater_than_second

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,14 @@
-# def values_greater_than_second(a):
-#     greater=[]
-#     for x in range(len(a)):
-#         if a[1] < a[x]:
-#             greater.append(a[x])
-#     if len(greater) == 0:
-#         return False
-#     else:
-#         print(len(greater))
-#         return(greater)
-# print(values_greater_than_second([5,2,3,2,1,4]))
+def values_greater_than_second(a):
+    greater=[]
+    for x in range(len(a)):
+        if a[1] < a[x]:
+            greater.append(a[x])
+    if len(greater) == 0:
+        return False
+    else:
+        print(len(greater))
+        return(greater)
+print(values_greater_than_second([5,2,3,2,1,4]))
 
 # def length_and_value(a,b):
 #     len=[]
