b433d1411dc723ea2f83f40696b62efceb4a2759
sdang92801 <77860420+sdang92801@users.noreply.github.com>
2021-01-24 13:41:46 -0800

    move hello_world under fundamentals

---
+++ fundamentals/hello_world.py
@@ -0,0 +1,23 @@
+print("Hello World")
+
+name = "Nichole"
+print("Hello",name)
+print('Hello'+name)
+
+name = 42
+print('Hello',name)
+# print('Hello'+name)
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}".format("sushi","pizza"))
+print(f'I love to eat {fave_food1} and {fave_food2}')
+
+# def cnt(a):
+#     x=[]
+#     while a !=0:
+#         x.append(a)
+#         a-=1
+#     x.append(0)
+#     return(x)
+# print(cnt(5))
--- hello_world.py
+++
@@ -1,8 +0,0 @@
-# def cnt(a):
-#     x=[]
-#     while a !=0:
-#         x.append(a)
-#         a-=1
-#     x.append(0)
-#     return(x)
-# print(cnt(5))
