087078c90afb62a8a683157f8ebc730ccddcac23
sdang92801 <77860420+sdang92801@users.noreply.github.com>
2021-02-11 16:19:42 -0800

    DVCreate Graph completed

--- fundamentals/DVrecreategraph.py
+++ fundamentals/DVrecreategraph.py
@@ -4,13 +4,16 @@ import numpy as np
 
 df = pd.read_csv(r'C:\Users\Employee\Downloads\mortgages.csv')
 ThreePercent = df.loc[(df['Interest Rate'] == 0.03) & (df['Mortgage Name'] == '30 Year'),:]
-FivePercent = df.loc[(df['Interest Rate'] == 0.03) & (df['Mortgage Name'] == '30 Year'),:]
-print(df.loc[ThreePercent,:])
-# Three_index = list(df.loc[ThreePercent,'Month'].values)
-# Three_value = list(df.loc[ThreePercent,:]['Interest Paid'].cumsum().values)
-# Five_index = list(df.loc[FivePercent,:]['Month'].values)
-# Five_value = list(df.loc[FivePercent,:]['Interest Paid'].cumsum().values)
+FivePercent = df.loc[(df['Interest Rate'] == 0.05) & (df['Mortgage Name'] == '30 Year'),:]
+Three_index = list(ThreePercent['Month'].values)
+Three_value = list(ThreePercent['Interest Paid'].cumsum().values)
+Five_index = list(FivePercent['Month'].values)
+Five_value = list(FivePercent['Interest Paid'].cumsum().values)
+print(Three_value)
+plt.plot(Three_index,Three_value,c='k',label="Three Percent")
+plt.plot(Five_index,Five_value,c='b',label="Five Percent")
+plt.xlabel('Months')
+plt.ylabel('Interest Paid')
+plt.title('Cumalative Interest Paid in 30 yrs')
 
-# plt.plot(Three_index,Three_value)
-# plt.plot(Five_index,Five_value)   
-# plt.show()
+plt.show()
